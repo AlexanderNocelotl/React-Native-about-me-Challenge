13acc834b9fc7ba77729bd03f680cff9159d1033
TXT Hackathon <txt.hackathon@TXTs-MacBook-Pro.local>
2024-07-12 12:06:02 -0700

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,87 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, View, Text, Platform } from 'react-native';
+import React from 'react';
+
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { rgbaArrayToRGBAColor } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
+
+
+
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    <ThemedView style= {styles.background}>
+      <View style={styles.divimage}>
+       <Image 
+        source={require('/Users/txt.hackathon/Desktop/React Native/profile/assets/images/IMG_4210 Small.jpeg')} 
+        style={styles.image} />
+        </View>
+<View> <Text style={styles.name}>Alexander Nocelotl</Text> </View>
+<View style={styles.container}>
+<Text style={styles.text}>
+  About Me:
+</Text>
+</View>
+
+<View style={styles.ocontainer}> <Text style={styles.passion}>My name is Alexander Nocelotl and I'm currently 16. My passions include things like books, splatoon, nintendo games, design, music, volleyball, and biking. The role I chose for SCLA 2024 is Developer. The reason I chose to be a dev is because I’ve always pushed away learning how to code because I thought it was difficult to understand. But this year I want to undertake the Dev role to better my skills and understand coding isn’t hard to understand. My dream career involves being a musical game composer for nintendo. I want to go to Japan and learn Japanese so I can become an employee at Nintendo of Japan. I want to become the person who is behind all the beautiful music at Nintendo and help create the next generation of music.
+</Text></View>
+ </ThemedView>);
+
+
+
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  background:{
+backgroundColor: 'white',
+  },
+  name:{
+    paddingTop: 20,
+    fontSize: 25,
+textAlign:'center',
+  },
+  ocontainer: {
+    justifyContent: 'center',
+  },
+  passion: {
+    fontSize: 15,
+    textAlign: "center",
+    padding: 30,
+    color: 'grey',
+  },
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  divimage:{
+paddingTop: 20,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  image: {
+    
+    borderWidth: 3,
+    borderColor: 'blue',
+    margin: 'auto',
+    borderRadius: 200,
+    width: 200,
+    height: 200,
   },
+
+  text: {
+    marginTop: 20,
+    fontSize: 20,
+    color: 'rgb(15, 93, 183)',
+    
+    },
+    
+
+
+ 
+
+
 });
+
